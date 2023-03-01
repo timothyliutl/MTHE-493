3fa417be6b0092396b65364c60d77f0862a4bffa
timothyliutl <18tfl1@queensu.ca>
2023-03-01 14:00:18 -0500

    almost done class quantization with channel error

--- quantizer_code/cosq_class.py
+++ quantizer_code/cosq_class.py
@@ -4,6 +4,7 @@ class CoSQ:
     def __init__(self, epsilon, bits):
         self.epsilon = epsilon
         self.bits = bits
+        self.centroids = []
 
     def training_set(self, training_input):
         self.training_set = training_input
@@ -11,7 +12,7 @@ class CoSQ:
     def fit(self):
 
         if not self.training_set:
-            raise Exception("No training set has been imported")
+            raise Exception("No training set has been imported you idiot sandwich")
 
         #train quantizer using lloyd max algorithm and memoryless channel
         num_centroids = 2**self.bits
@@ -40,28 +41,49 @@ class CoSQ:
                     prob = prob * self.epsilon
             return prob
 
-
+        def calc_expected_distortion(centroids, index, point):
+            distortion = 0
+            for i in range(len(centroids)):
+                trans_prob = calc_transition_prob(index, i)
+                distortion = distortion + trans_prob*((centroids[i] - point)**2)
+            return distortion
 
         def calc_partitions(points, centroids):
             partition_output = {centroid:[] for centroid in centroids}
             print(partition_output)
             for index, point in enumerate(points):
                 closest_num = takeClosest(point, centroids)
-                partition_output[closest_num].append(point)
+                # centroid the quantizer would quantize to without a noisy channel
+                # convert it to a number
+                # maybe we can reorganize it so the items with the highest distortion have the lowest probabilities
+                
+                #index/number the point is quantized to
+                index_centroid = centroids.index(closest_num)
+                #array that calculates all the mean square distances for each centroid
+                mse_array = []
+                for input_index in range(len(centroids)):
+                    #calculating expected distortion for a given quantization, input_index
+                    mse_array.append(calc_expected_distortion(centroids, input_index, point))
+                mse_array = np.array(mse_array)
+                #find quantization with smallest distortion and append quantization to corresponding centroid in partition output
+                index_smallest_distortion = np.argmin(mse_array)
+                partition_output[centroids[index_smallest_distortion]].append(point)
             return partition_output
         
         def calc_centroids(partitions, centroids):
             new_centroids = []
-            for centroid in centroids:
-                new_centroids.append(np.array(partitions[centroid]).mean())
+            for index, centroid in enumerate(centroids):
+                weighted_average = 0
+                for i in range(len(centroids)):
+                    trans_prob = calc_transition_prob(index, i)
+                    weighted_average = weighted_average + trans_prob*((np.array(partitions[centroid])).mean())
+                new_centroids.append(weighted_average)
             return new_centroids
-        
-        
         
         def iteration():
             pass
-
-        return calc_transition_prob(10,12)
+        #return calc_partitions(self.training_set, centroids)
+        return calc_centroids(calc_partitions(self.training_set, centroids), centroids)
             
 
     def quantize(self, value):
