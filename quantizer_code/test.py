af61c137d5c4272652a83a3d1e7c94b8b603c855
timothyliutl <18tfl1@queensu.ca>
2023-03-01 19:27:24 -0500

    changing file structure and importing quantizer to notebook

--- quantizer_code/test.py
+++ quantizer_code/test.py
@@ -1,8 +1,10 @@
 from cosq_class import CoSQ
 import numpy as np
 
-obj = CoSQ(0.05, 2)
+obj = CoSQ(0.05, 1)
 obj.training_set([np.random.randint(0,255) for i in range(100)])
-
-print(obj.fit())
+obj.fit()
+print(obj.centroids)
+print(obj.centroid_map)
+print(obj.quantize(200))
 
