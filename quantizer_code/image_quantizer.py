daee8e4aecff56b1efd4e42b01488a4eb9f12661
timothyliutl <18tfl1@queensu.ca>
2023-03-01 19:37:48 -0500

    new file to tie together quantization code

---
+++ quantizer_code/image_quantizer.py
@@ -0,0 +1,14 @@
+# creating a class to take in an image training set and compress given images
+
+class ImageQuantizer:
+    def __init__(self, bit_al_mat):
+        self.bit_allocation_matrix = bit_al_mat
+
+    def training_set(self, images):
+        pass
+
+    def train(self):
+        pass
+
+    def compress_image(self, image):
+        pass
