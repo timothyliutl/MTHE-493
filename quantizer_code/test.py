2fbb43a2d0e52b53357aa8a430a92368008b154b
mfilly <mfillmore2000@gmail.com>
2023-03-14 13:38:38 -0400

    Fix model comments

--- quantizer_code/test.py
+++ quantizer_code/test.py
@@ -6,13 +6,13 @@ import cv2
 import matplotlib.pyplot as plt
 
 
-arr = [np.random.rand()*1000 for i in range(1000)]
-
-
-
-obj = CoSQ(0.05, 1)
-obj.training_set([1,2,3,4,5,6,7,8,9,10])
-obj.fit()
+#arr = [np.random.rand()*1000 for i in range(1000)]
+#
+#
+#
+#obj = CoSQ(0.05, 1)
+#obj.training_set([1,2,3,4,5,6,7,8,9,10])
+#obj.fit()
 
 
 bit_al_mat = np.matrix([[6,4,3,1,0,0,0,0],
@@ -33,7 +33,7 @@ obj2 = ImageQuantizer(bit_al_mat, epsilon)
 obj2.import_training_set('../data/org/')
 image = cv2.imread('../data/org/1.pgm')
 obj2.train()
-obj2.save_model('./model_01', "#Epsilon = {}".format(epsilon))
+obj2.save_model('./model_01', ["#Epsilon = {}".format(epsilon)])
 #obj2.load_model('./model')
 compressed_img = obj2.compress_image(image)
 channel = Channel(epsilon, bit_al_mat)
